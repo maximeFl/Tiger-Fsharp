3b787abdb5531b5ff7f15e1d751cf606d2554922
Maxime Flauder <max.flauder@gmail.com>
2016-12-30 19:13:35 +0100

    interpret compile

--- ch01/sml.fs
+++ ch01/sml.fs
@@ -20,29 +20,38 @@ let eval x y = function
   | Times -> x * y
 
 
-
 let rec lookup value (mytable : table) =
   match mytable with 
-    | [] -> None
     | (s,i)::tail -> if  s = value then 
-                       Some i 
+                       i 
                      else
                        lookup value tail
+    | _ -> 0
 
 let update_table (mytable : table) value_id value = 
   (value_id, value)::mytable
 
-let print_stm i (mytable: table) =
+let print_stm (i:int , mytable: table) =
   printfn "%i" i
   mytable 
 
-let interpExp (exp_a: exp) (table_a: table) =
-  // exp -> tablea -> (int * table)
-  (5, ("a", 5))  
+//let interpExp (exp_a: exp) (table_a: table) =
+//  // exp -> tablea -> (int * table)
+//  (5, [("a", 5)])  
 
 
-let rec interpStm mytable = function 
+
+let rec interpStm mytable = function
   | CompoundStm(stm_a, stm_b) -> interpStm (interpStm mytable stm_a) stm_b
-  | AssignStm(id_a, exp_a) -> update_table (interpExp exp_a mytable) id_a
-  | PrintStm stm_list -> List.fold (fun t e -> print_stm (interpExp e t))  mytable stm_list
+  | AssignStm(id_a, exp_a) -> let mynumber, mytable = interpExp exp_a mytable
+                              update_table mytable id_a mynumber
+  | PrintStm stm_list -> List.fold (fun t e -> print_stm (interpExp e t ))  mytable stm_list
 
+and interpExp (exp_a: exp) (table_a: table) =
+  match exp_a with 
+    | IdExp id_a -> lookup  id_a table_a, table_a
+    | NumExp int_a -> int_a, table_a
+    | OpExp(exp_1, op,  exp_2) -> let  int_exp_1, table_a = interpExp exp_1 table_a
+                                  let  int_exp_2, table_a = interpExp exp_2 table_a
+                                  eval int_exp_1 int_exp_2 op, table_a
+    | EseqExp(stm_a, exp_a) -> interpExp exp_a (interpStm stm_a)   
