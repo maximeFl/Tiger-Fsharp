2344e02c42e21ff973f17e5d146f0c9697109694
Maxime Flauder <max.flauder@gmail.com>
2017-05-07 19:39:31 +0200

    add update autom

--- ch02/regex.fs
+++ ch02/regex.fs
@@ -45,15 +45,48 @@ let rec follow c = function
                     if last_a.Contains c then Set.union follow_a_b first_b else follow_a_b  
 
 let next_step r state c  =
-  let folding_function q = function 
+  let foldingFunction q = function 
     | (c1,_) as c' -> if c1 = c then Set.union (follow c' r) q else q
-  Set.fold folding_function Set.empty<ichar> state  
-
-
+  Set.fold foldingFunction Set.empty<ichar> state  
 
 type state = Set<ichar>
-
+type trans = Map<state*char, state>
 type autom = {
   start : state;
-  trans : state Cmap.t Smap.t
+  trans : trans;
+  regex : regexp
 }
+
+
+let rec update f x =
+  let to_continue, new_x = f x 
+  if to_continue then update f new_x else new_x
+
+let update_characters (s:state) (all_characters:Set<char>) (r:regexp) =
+  Set.fold (fun trans c -> let ns =(next_step r s c)
+                           if Set.isEmpty ns then
+                            trans
+                           else
+                            Map.add (s, c) (next_step r s c) trans)  Map.empty<state*char, state> all_characters
+
+
+let allCharacter (s:state) =
+  Set.map fst s
+
+let updateAutom (a:autom)  = 
+  let start = a.start
+  let trans = a.trans
+  let r = a.regex
+  let all_states =  Map.fold (fun a b c -> Set.union a (Set.union (Set.singleton c) (Set.singleton (fst b)))) Set.empty<state> trans
+                    |> Set.add start
+  let all_characters = Set.toSeq all_states
+                       |>Seq.collect (fun state -> Set.map fst state |> Set.toSeq) 
+                       |>Set.ofSeq 
+  let new_trans = Set.toList all_states 
+                  |> List.map (fun state -> update_characters state all_characters r)
+                  |> List.append [a.trans]
+                  |> List.collect Map.toList
+                  |> Map.ofList
+  new_trans = trans, new_trans
+                  
+
